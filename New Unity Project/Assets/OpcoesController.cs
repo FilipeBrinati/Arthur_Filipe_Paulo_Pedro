e59ad7f000e63cad128ca155fdf80bc824767468
Paulo Rozatto <rozatto.paulo.victot@gmail.com>
2022-06-01 18:46:01 -0300

    resolucoes alteraveis

--- New Unity Project/Assets/OpcoesController.cs
+++ New Unity Project/Assets/OpcoesController.cs
@@ -3,12 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class OpcoesController : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public TMP_Dropdown resolutionDropdown;
 
-    public void volumeHandler(float volume){
+    Resolution[] resolutions;
+
+    void Start() {
+        resolutions = Screen.resolutions;
+        List<string> options = new List<string>();
+
+        resolutionDropdown.ClearOptions();
+
+        int currResolution = resolutions.Length - 1;
+        for(int i = 0; i < resolutions.Length; i++) {
+            string opt = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(opt);
+                
+            if(resolutions[i].width == Screen.currentResolution.width && 
+               resolutions[i].height == Screen.currentResolution.height)
+                currResolution = i;
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currResolution;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int index) {
+        Resolution resol = resolutions[index];
+        Screen.SetResolution(resol.width, resol.height, true);
+    }
+
+    public void SetVolume(float volume){
         audioMixer.SetFloat("volume", volume);
     }
 
